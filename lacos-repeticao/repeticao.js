6395f136668d4161c114bec87ffae74ef9a9d6b9
mfernandasilva <mariasalomao2002@gmail.com>
2024-04-11 15:16:59 -0300

    repeticao

---
+++ lacos-repeticao/repeticao.js
@@ -0,0 +1,38 @@
+/* let tabuada = Number(prompt("Insira a tabuada que voce deseja "))
+
+for(let contador = 0; contador <= 20; contador ++){
+    console.log(tabuada + " + " + contador + " = " + contador*tabuada)
+}
+*/
+
+
+/* 1 - Faça um algoritmo que receba um valor, inteiro e positivo, e usando a estrutura FOR, mostre no console uma contagem de 0 até o número recebido. 
+
+let numero = Number(prompt("Informe um numero inteiro e positivo "))
+
+for(let i = 0; i <= numero; i ++){
+    console.log(i)
+}
+*/
+
+
+/* 2 - Desenvolva um algoritmo que usando a estrutura FOR mostre no console uma contagem de 0 a 50, porém, essa contagem tem que ser feita de 5 em 5.
+   Ex.: 0 5 10 15 20 25 … 45 50 Fim! 
+
+   for(let i = 0; i <= 50; i +=5){
+    console.log(i)
+}
+*/
+
+
+/* 3 - Agora faça com que o algoritmo do exercício anterior exiba a mesma contagem mas na ordem inversa.
+   Ex.: 50 45 40 35 30 25 … 5 0 Fim! 
+
+   
+   for(let i = 50; i >= 0; i -=5){
+    console.log(i)
+   }
+   */
+
+
+   
---
+++ src/script.js
@@ -0,0 +1,7 @@
+/* 3 - Agora faça com que o algoritmo do exercício anterior exiba a mesma contagem mas na ordem inversa.
+   Ex.: 50 45 40 35 30 25 … 5 0 Fim! */
+
+   
+   for(let i = 50; i >= 0; i -=5){
+    console.log(i)
+   }
