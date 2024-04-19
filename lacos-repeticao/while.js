f0329699dcb9e4cbc998ea9604d1bce514347257
mfernandasilva <mariasalomao2002@gmail.com>
2024-04-18 23:20:48 -0300

    Resoluções exercicios while e do while

--- lacos-repeticao/while.js
+++ lacos-repeticao/while.js
@@ -1,3 +1,91 @@
+/* Escreva um programa que imprima os números de 1 a 10. 
+
+let contador = 1;
+
+while (contador <= 10) { 
+    console.log(contador);
+    contador++;
+}
+*/
+
+
+/* Escreva um programa que solicite ao usuário um número e imprima todos os números de 1 até esse número. 
+ 
+let numero = Number(prompt("Digite um número: "));
+let contador = 1;
+
+while (contador <= numero) {
+    console.log(contador);
+    contador++;
+}
+*/
+
+
+/* Escreva um programa que imprima os números pares de 1 a 20. 
+
+let contador = 1;
+
+while (contador <= 20) {
+    if (contador % 2 === 0) {
+        console.log(contador);
+    }
+    contador++;
+}
+*/
+
+
+/* Escreva um programa que solicite ao usuário uma senha e continue pedindo até que a senha correta seja fornecida (a senha correta é "senha123"). 
+
+let senha = prompt("Digite a senha: ");
+
+while (senha !== "senha123") {
+    senha = prompt("Senha incorreta. Por favor, tente novamente: ");
+}
+
+console.log("Senha correta. Acesso concedido!");
+*/
+
+
+// do while //
+
+/*
+let numero;
+
+do {
+    numero = Number(prompt("Digite um número positivo: "));
+} while (numero <= 0);
+
+console.log("Número positivo fornecido:", numero);
+*/
+
+
+/* let senhaCorreta = "senha123";
+let senhaDigitada;
+
+do {
+    senhaDigitada = prompt("Digite a senha: ");
+} while (senhaDigitada !== senhaCorreta);
+
+console.log("Acesso concedido!");
+*/
+
+
+/* Escreva um programa que solicite ao usuário um número e imprima todos os números de 1 até esse número. */
+
+
+/*let contador = 1;
+let numero;
+do {
+    numero = parseInt(prompt("Digite um número: "));
+} while (isNaN(numero) || numero <= 0);
+
+while (contador <= numero) {
+    alert(contador);
+    contador++;
+}
+*/
+ 
+
 /* Solicite o nome e o cpf do seu cliente. Pergunte um valor.
 Ofereça a opção de saque ou depósito.
 Caso a opção de saque seja a escolhida, remova o valor inserido do saldo total.
@@ -10,7 +98,7 @@ Exiba a média dos valores inseridos(independente de ser depósito ou saque).
 REGRAS: 
     1- Considere iniciar a variável saldo com algum valor
     1- Não deve ser permitido sacar um valor não disponível no saldo.
-    2- Não deve ser permitido sacar nem depositar um valor negativo. */
+    2- Não deve ser permitido sacar nem depositar um valor negativo. 
 
     let saldo = 1000;
     let maiorValorInserido = 0;
@@ -68,3 +156,4 @@ REGRAS:
     alert(`Saldo final: R$${saldo}`);
     alert(`Maior valor inserido: R$${maiorValorInserido}`);
     alert(`Média dos valores inseridos: R$${(somaValoresInseridos / totalTransacoes).toFixed(2)}`);
+*/
