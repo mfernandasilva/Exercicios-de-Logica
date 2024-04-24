8dd6906aead81e34d6e922c9f9d714bf0f432ff9
mfernandasilva <mariasalomao2002@gmail.com>
2024-04-24 15:05:16 -0300

    fixacao

---
+++ fixacao/fixacao.js
@@ -0,0 +1,91 @@
+/* Escreva um programa que some dois números fornecidos pelo usuário e exiba o resultado. 
+
+let n1 = Number(prompt("Informe um numero: "))
+let n2 = Number(prompt("Informe outro numero: "))
+
+let soma = n1 + n2;
+
+alert("A soma dos dois numeros fornecidos é igual a: " + soma);
+*/
+
+
+/* Escreva um programa que peça ao usuário seu nome e exiba uma mensagem de saudação. 
+
+let nome = prompt("Qual o seu nome: ")
+
+alert("Olá, " + nome + " seja Bem-Vindo(a). Estamos contentes em ter você aqui.")
+*/
+
+
+/* Escreva um programa que peça ao usuário dois números e exiba o maior número. 
+
+let n1 = Number(prompt("informe um numero: "))
+let n2 = Number(prompt("informe outro numero: "))
+
+if(n1 > n2){
+    alert("O ,aior numero informado foi: " + n1)
+}
+else {
+    alert("O ,aior numero informado foi: " + n2)
+}
+*/
+
+
+/* Crie um programa que converta graus Celsius para Fahrenheit. 
+
+const celsius = parseFloat(prompt("Digite a temperatura em graus Celsius:"));
+
+const fahrenheit = celsius * 9 / 5 + 32;
+
+console.log(`${celsius} graus Celsius é igual a ${fahrenheit.toFixed(2)} graus Fahrenheit.`);
+*/
+
+
+/* Escreva um programa que verifique se um número fornecido pelo usuário é par ou ímpar.
+
+let n1 = Number(prompt("Informe um numero: "))
+
+if(n1 % 2 == 0){
+    alert("O numero fornecido é par")
+}
+else{
+    alert("O numero fornecido é impar")
+}
+*/
+
+
+/* Escreva um programa que calcule a área de um círculo dado seu raio. 
+
+let raio = Number(prompt("Insira o raio do círculo:"));
+
+let areaDoCirculo = Math.PI * Math.pow(raio, 2);
+
+alert("A área do círculo é igual a: " + areaDoCirculo.toFixed(2));
+
+*/
+
+
+/* Escreva um programa que imprima os números de 1 a 10. 
+
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
+*/
+
+
+/* Crie um programa que some todos os números de 1 a 100. 
+
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
+ */
+
+
+/* Escreva um programa que imprima os números de 10 a 1. 
+
+for(let i = 10; i >= 1; i--){
+    console.log(i)
+}
+ */
+
+
