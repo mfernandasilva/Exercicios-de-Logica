fa62c73b227858d976ad2ab441db909a6836065a
mfernandasilva <mariasalomao2002@gmail.com>
2024-08-19 12:13:59 -0300

    pratica loop

--- src/script.js
+++ src/script.js
@@ -1,14 +1,2 @@
-// Imprime a sequência de Fibonacci até o décimo termo.
 
-let termoAnterior = 0
-let termoAtual = 1
 
-console.log(termoAnterior)
-console.log(termoAtual)
-
-for (let i = 3; i <= 10; i++) { 
-    let proximoTermo = termoAnterior + termoAtual
-    console.log(proximoTermo)
-    termoAnterior = termoAtual
-    termoAtual = proximoTermo
-}
