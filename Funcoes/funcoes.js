5169e1b9f039810dca60607e16a0d97d86b847b8
Maria Fernanda <maria.salomao.1478249@sga.pucminas.br>
2024-09-02 11:32:44 -0300

    funcoes

--- Funcoes/funcoes.js
+++ Funcoes/funcoes.js
@@ -1,3 +1,4 @@
+/*  Aula sobre funções 
 function somar(n1, n2){
      console.log(n1)
      console.log(n2)
@@ -5,3 +6,6 @@ function somar(n1, n2){
 }
 
 somar(25, 80);
+*/
+
+
--- lacos-repeticao/while.js
+++ lacos-repeticao/while.js
@@ -278,3 +278,13 @@ console.log("Parabéns! Você adivinhou o número.");
 */
 
 
+/* 
+let idade = 18
+while(idade >= 18) {
+     idade = Number(prompt("Insira sua idade: "))
+     console.log(idade)
+}
+console.log("Acabou o while")
+*/
+
+
