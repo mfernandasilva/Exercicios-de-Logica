b9e2703683a22e1bcaab4e43ff352891476c45fd
Maria Fernanda <maria.salomao.1478249@sga.pucminas.br>
2024-09-12 09:44:16 -0300

    Praticando

--- Funcoes/funcoes.js
+++ Funcoes/funcoes.js
@@ -279,3 +279,27 @@ imcUsuario();
 */
 
 
+/* 1 - Conhecendo seu usuário
+Agora você deve exibir todos os anos que o usuário já viveu até o ano atual e informar 
+qual a idade dele no determinado ano.
+   Ex: Nasceu em 2018 você deve exibir: 
+       2018 - 0 anos de idade
+       2019 - 1 anos de idade
+       2020 - 2 anos de idade
+       2021 - 3 anos de idade
+       2022 - 4 anos de idade
+
+function idadeUsuario () {
+    const anoAtual = 2024;
+    const idade = Number(prompt("Informe sua idade: "));
+    const anoNascimento = anoAtual - idade;
+
+    for (let ano = anoNascimento; ano <= anoAtual; ano++) {
+        console.log(`${ano} - ${ano - anoNascimento} anos de idade`);
+    }
+}
+
+idadeUsuario();
+*/
+
+
--- src/script.js
+++ src/script.js
@@ -1,43 +1,25 @@
 /* 1 - Conhecendo seu usuário
-Calcule o IMC do seu usuário e informe a faixa dele.
-*O IMC é a relação entre peso e altura e o cálculo é feito de acordo com a 
-fórmula: IMC = peso / (altura x altura)
-O peso deve estar em kg e a altura em metros, e o resultado é dado em kg/m2. 
-Depois de obter o resultado, é possível verificar em que faixa o IMC se encontra, 
-podendo indicar:
-      - Magreza, quando o resultado é menor que 18,5 kg/m2;
-      - Normal, quando o resultado está entre 18,5 e 24,9 kg/m2;   
-      - Sobrepeso, quando o resultado está entre 24,9 e 30 kg/m2;
-      - Obesidade, quando o resultado é maior que 30 kg/m2.
-A partir da idade informada descubra o ano que o seu usuário nasceu(Considere o ano 
-atual como 2024, faça essa conta usando apenas o ano). */
+Agora você deve exibir todos os anos que o usuário já viveu até o ano atual e informar 
+qual a idade dele no determinado ano.
+   Ex: Nasceu em 2018 você deve exibir: 
+       2018 - 0 anos de idade
+       2019 - 1 anos de idade
+       2020 - 2 anos de idade
+       2021 - 3 anos de idade
+       2022 - 4 anos de idade
+*/
 
-function imcUsuario () {
-    const peso = parseFloat(prompt("Informe seu peso (em kg): "));
-    const altura = parseFloat(prompt("Informe sua altura (em metros): "));
-
-    // Calculando o IMC
-    const imc = peso / (altura * altura);
-
-    if (imc < 18.5) {
-        console.log("Magreza");
-    } 
-    else if (imc >= 18.5 && imc <= 24.9) {
-        console.log("Normal");
-    } 
-    else if (imc > 24.9 && imc <= 30) {
-        console.log("Sobrepeso");
-    } 
-    else {
-        console.log("Obesidade");
-    }
-
-    // Calculando o ano de nascimento com base na idade e ano atual
-    const idade = Number(prompt("Informe sua idade: "));
+function idadeUsuario () {
     const anoAtual = 2024;
+    const idade = Number(prompt("Informe sua idade: "));
     const anoNascimento = anoAtual - idade;
 
-    console.log(`Você nasceu em ${anoNascimento}.`);
+    for (let ano = anoNascimento; ano <= anoAtual; ano++) {
+        console.log(`${ano} - ${ano - anoNascimento} anos de idade`);
+    }
 }
-imcUsuario();
+
+idadeUsuario();
+
+
 
